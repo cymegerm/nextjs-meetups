96d3d8aeba356bb9ec6fd612333228149e954473
Simon St-Germain <cymegerm@gmail.com>
2023-09-26 02:52:59 -0400

    add paris and london meetup data

--- app/components/fakeMeetups.ts
+++ app/components/fakeMeetups.ts
@@ -3,7 +3,7 @@ import { Meetup } from '@/app/components/meetup-item';
 export const fakeMeetups: Meetup[] = [
   {
     id: 'm1',
-    title: 'Montreal, Canada',
+    title: 'Montreal – Canada',
     imageUrl:
       'https://upload.wikimedia.org/wikipedia/commons/4/4f/Montreal-canada-parc-urban.jpg',
     address: 'École de technologie supérieure ÉTS',
@@ -11,7 +11,7 @@ export const fakeMeetups: Meetup[] = [
   },
   {
     id: 'm2',
-    title: 'San Francisco, USA',
+    title: 'San Francisco – USA',
     imageUrl:
       'https://upload.wikimedia.org/wikipedia/commons/6/61/San_Francisco_from_the_Marin_Headlands_in_August_2022.jpg',
     address: 'Carnegie Mellon University Silicon Valley',
@@ -19,7 +19,23 @@ export const fakeMeetups: Meetup[] = [
   },
   {
     id: 'm3',
-    title: 'Berlin, Germany',
+    title: 'London – UK',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/1/14/City_of_London_skyline_27.12.2019.jpg',
+    address: 'University College London',
+    imageAlt: 'skyline of London',
+  },
+  {
+    id: 'm4',
+    title: 'Paris – France',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/0/08/Seine_and_Eiffel_Tower_from_Tour_Saint_Jacques_2013-08.JPG',
+    address: 'Institut Polytechnique de Paris',
+    imageAlt: 'skyline of Paris',
+  },
+  {
+    id: 'm5',
+    title: 'Berlin – Germany',
     imageUrl:
       'https://upload.wikimedia.org/wikipedia/commons/6/6c/Aerial_view_of_Berlin_%2832881394137%29.jpg',
     address: 'Technical University of Berlin',
