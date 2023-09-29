a4ee36ac09fa42049e4c0f13c919045a1e3784ba
Simon St-Germain <cymegerm@gmail.com>
2023-09-29 04:05:24 -0400

    create base64 placeholder images

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        TODO: Add Navigation Menu
+        {/*TODO: Add Navigation Menu*/}
         {children}
       </body>
     </html>
