3d1423754b7852fe50d63bb1e7415db577e5b176
Simon St-Germain <cymegerm@gmail.com>
2023-10-08 10:23:02 -0400

    add large underline to nav-menu logo on hover

--- app/components/nav-menu.tsx
+++ app/components/nav-menu.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const NavMenu = () => {
   return (
-    <nav className='sticky top-0 left-0 width-screen p-6 text-2xl font-bold backdrop-blur-3xl'>
+    <nav className='sticky top-0 left-0 width-screen p-6 text-2xl backdrop-blur-3xl'>
       <Link
         href='/'
-        className='italic focus:opacity-90 active:opacity-90 visited:opacity-100 hover:opacity-90 transition:opacity'
+        className='italic tracking-widest decoration-transparent decoration-4 underline-offset-8 hover:underline focus:opacity-90 active:opacity-90 visited:opacity-100 hover:opacity-90 hover:decoration-stone-700/90 transition-all'
       >
         Next Meetups
       </Link>
