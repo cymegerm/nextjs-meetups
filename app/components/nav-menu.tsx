7801c3711ac66a8192e170f96025321c99e43870
Simon St-Germain <cymegerm@gmail.com>
2023-10-08 09:14:57 -0400

    make nav-menu logo text italic

--- app/components/nav-menu.tsx
+++ app/components/nav-menu.tsx
@@ -5,7 +5,7 @@ const NavMenu = () => {
     <nav className='sticky top-0 left-0 width-screen p-6 text-2xl font-bold backdrop-blur-3xl'>
       <Link
         href='/'
-        className='focus:opacity-90 active:opacity-90 visited:opacity-100 hover:opacity-90 transition:opacity'
+        className='italic focus:opacity-90 active:opacity-90 visited:opacity-100 hover:opacity-90 transition:opacity'
       >
         Next Meetups
       </Link>
