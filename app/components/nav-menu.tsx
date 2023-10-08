263cad277423dddbae1aec089c992ee31ec9cb98
Simon St-Germain <cymegerm@gmail.com>
2023-10-08 09:09:08 -0400

    add box-shadow to 'card' component + add backdrop-blur to nav-menu

--- app/components/nav-menu.tsx
+++ app/components/nav-menu.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const NavMenu = () => {
   return (
-    <nav className='p-6 text-2xl font-bold sticky top-0 left-0 width-screen'>
+    <nav className='sticky top-0 left-0 width-screen p-6 text-2xl font-bold backdrop-blur-3xl'>
       <Link
         href='/'
         className='focus:opacity-90 active:opacity-90 visited:opacity-100 hover:opacity-90 transition:opacity'
