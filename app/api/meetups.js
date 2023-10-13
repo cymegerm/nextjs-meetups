a8092508ee54de9cb1386f604a4b0aef7bb228fe
Simon St-Germain <cymegerm@gmail.com>
2023-10-13 02:43:05 -0400

    add todo comment

--- app/api/meetups.js
+++ app/api/meetups.js
@@ -1,4 +1,4 @@
-/* TODO: Refactor with DB call */
+/* TODO: Refactor with DB call directly in meetup-list.tsx via Drizzle */
 
 export async function getMeetups() {
   return await fetch('/fakeMeetups.json');
